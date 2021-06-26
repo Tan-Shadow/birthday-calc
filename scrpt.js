bda7a724dfa9bb7e2d9f28666f9cda2b209a4cf3
TanShadow <kagekun018@gmail.com>
2021-06-26 11:01:44 +0530

    Initial commit

---
+++ scrpt.js
@@ -0,0 +1,42 @@
+const daysEle = document.querySelector('.days')
+const hoursEle = document.querySelector('.hours')
+const minEle = document.querySelector('.minutes')
+const secEle = document.querySelector('.seconds')
+
+
+
+// runs when user presses btn
+function getInp(){
+    
+    var userInput = document.querySelector('.userInput').value
+    const userDate = new Date(userInput)
+    // console.log(userDate)
+
+    if (userInput != null){
+
+        function timeCalc(){
+            let today = new Date()
+    
+            // difference between today and userDate in seconds
+            const diffInSec = (userDate - today) / 1000
+    
+            let days = Math.floor((diffInSec / 3600) / 24)
+            let hours = Math.floor((diffInSec / 3600) % 24)
+            let minutes = Math.floor((diffInSec / 60) % 60)
+            let seconds = Math.floor((diffInSec % 60))
+            
+            daysEle.innerHTML =  days
+            hoursEle.innerHTML =  hours
+            minEle.innerHTML =  minutes
+            secEle.innerHTML =  seconds
+            
+            console.log(days, hours, minutes, seconds)
+    
+        }
+        setInterval(timeCalc, 1000)
+    }
+}
+
+
+
+
